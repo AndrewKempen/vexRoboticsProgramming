c6dd487b703a3159f959e5856f8de447c1b5a532
Wishbonea <wishbone.jung@hotmail.com>
2014-04-01 08:39:29 -0500

    Fixed Misspelling, Added More Variables
    Also made it a system file

--- globalVariables.h
+++ globalVariables.h
@@ -1,3 +1,5 @@
+#pragma systemFile
+
 const int maxSpeed = 127;
 const int minSpeed = -127;
 const float MAINBATTERYTHRESHOLD = 7.2;
@@ -5,7 +7,7 @@ const float SECONDBATTERYTHRESHOLD = 7.2;
 const float BACKUPBATTERYTHRESHOLD = 9;
 int powerExpanderLevel;
 bool IMEFAILURE = false;
-int atonNumber;
+int autonNumber;
 string parameter;
 string line1;
 string line0;
@@ -13,8 +15,28 @@ int MOD = 1;
 bool intakeOn = false;
 bool intakeBtnReleased = false;
 
+int liftBtnUpLastState;
+int liftBtnDownLastState;
+
 bool armIsDown = true;
 int threshold = 1852;
+
+typedef enum
+{
+	none = 00,
+	redHanging = 10,
+	redMiddle = 20,
+	blueHanging = 30,
+	blueMiddle = 40,
+}autonZoneCode;
+
+typedef enum
+{
+	none = 0,
+	hang = 1,
+	stash = 2,
+	moveBigBalls = 3,
+}autonCode;
 
 struct{
 	bool isDown;
