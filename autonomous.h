81e2e9ecbc76c1f15742218b1f824769426d185d
Wishbonea <wishbone.jung@hotmail.com>
2014-04-06 13:27:59 -0500

    Finished up Autonomous Framework

--- autonomous.h
+++ autonomous.h
@@ -15,42 +15,71 @@
 +--------+---------------------+--------------------+--------------------+
 | 4      | Blue Middle Zone    |                    |                    |
 +--------+---------------------+--------------------+--------------------+
-| 5      | Programing Skills 1 | Retreat From Right | Retreat From Left  |
+| 5      | Programing Skills   | Retreat From Right | Retreat From Left  |
 +--------+---------------------+--------------------+--------------------+
 */
-void hangingZone(bool red)
+
+void hangingZoneHang(bool red)
+{
+
+}
+void hangingZoneMoveBigBall(bool red)
+{
+
+}
+void middleZoneStash(bool red)
+{
+
+}
+void middleZoneGetBigBall(bool red)
 {
 
 }
 
 void auton(int autonomusCode)
 {
+	bool red;
 	int zone = (autonomusCode/10);
-	switch(zone)
+	if(zone == 1 || zone == 2)
 	{
-	case redHangingZone:
-		switch(autonomusCode - zone)
+		red = true;
+	}
+	else
+	{
+		red = false;
+	}
+	if(zone == 1 || zone == 3)
+	{
+		switch(autonomusCode - (zone*10))
 		{
 		case hangAuton:
+			hangingZoneHang(red);
 			return;
 		case moveBigBallAuton:
+			hangingZoneMoveBigBall(red);
 			return;
 		default:
 			writeDebugStreamLine("ERROR: Unexpected auton number at \"auton\"");
 			return;
 		}
-		return;
-	case redMiddleZone:
-
-		return;
-	case blueHangingZone:
-
-		return;
-	case blueMiddleZone:
-
-		return;
-	default:
+	}
+	else if(zone == 2 || zone == 4)
+	{
+		switch(autonomusCode - (zone*10))
+		{
+		case stashAuton:
+			middleZoneStash(red);
+			return;
+		case getBigBallAuton:
+			middleZoneGetBigBall(red);
+			return;
+		default:
+			writeDebugStreamLine("ERROR: Unexpected auton number at \"auton\"");
+			return;
+		}
+	}
+	else
+	{
 		writeDebugStreamLine("ERROR: Unexpected zone number at \"auton\"");
-		return;
 	}
 }
