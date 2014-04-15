c0921763fb2a578958fd7a926609cf508321eba1
Wishbonea <wishbone.jung@hotmail.com>
2014-04-15 10:28:18 -0500

    Fixes, Base PID Variables Added

--- globalVariables.h
+++ globalVariables.h
@@ -10,6 +10,7 @@ int autonNumber;
 string parameter;
 string line1;
 string line0;
+int screen;
 int MOD = 1;
 bool intakeOn = false;
 bool intakeBtnReleased = false;
@@ -19,6 +20,7 @@ int liftBtnDownLastState;
 
 bool armIsDown = true;
 int threshold = 1852;
+int farThreshold = 1852;
 
 const int	noZone = 00;
 const int	redHangingZone = 10;
@@ -28,8 +30,9 @@ const int	blueMiddleZone = 40;
 const int	noAuton = 0;
 const int	hangAuton = 1;
 const	int stashAuton = 2;
-const int	moveBigBallAuton = 3;
-const int getBigBallAuton = 4;
+const int hangingZoneStashAuton = 3;
+const int	moveBigBallAuton = 4;
+const int getBigBallAuton = 5;
 
 struct{
 	bool isDown;
@@ -37,25 +40,59 @@ struct{
 	int stashHeight;
 	int tipHeight;
 	int location;
+	int lastLocation;
 	bool isDown;
 	bool isMax;
 	bool isPIDon;
-	bool isauton;
-	int requestedLocation;
-	int error;
-	int lastError;
+	bool isManualPos;
+	bool isJammed;
+	float requestedLocation;
+	float error;
+	float lastError;
 	float kp;
 	float kd;
 } liftStruct;
-liftStruct lift;
+static liftStruct lift;
+
+
+struct{
+	float requestedLocation;
+	float error;
+	float lastError;
+} rightStruct;
+static rightStruct right;
+
+struct{
+	float requestedLocation;
+	float error;
+	float lastError;
+} leftStruct;
+static leftStruct left;
+
+struct{
+	bool isPIDon;
+	float kp;
+	float kd;
+} baseStruct;
+static baseStruct robot;
 
 void liftPIDValues()
 {
-	lift.kp = 0.1;
-	lift.kd = 0.1;
+	lift.kp = 2;
+	lift.kd = 0.4;
+	lift.lastError = 0;
+}
+void robotPIDValues()
+{
+	robot.kp = 0.6;
+	robot.kd = 0.2;
+	right.lastError = 0;
+	left.lastError = 0;
 }
 void liftValues() //Set Hanging Arm Maximum
 {
+	liftPIDValues();
+	robotPIDValues();
 	lift.maxHeight = 1790;
-	lift.tipHeight = ((lift.maxHeight/3)*2);
+	lift.tipHeight = 500;
 }
